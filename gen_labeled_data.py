5386cd956ceb2cdfcf800577ddf4d20d3a0044f8
Kenny Piccirilli-Platt <kpiccirilliplatt@gmail.com>
2026-04-21 14:44:39 -0700

    update data gen to ignore more features

--- gen_labeled_data.py
+++ gen_labeled_data.py
@@ -32,7 +32,7 @@ def gen_labeled_data():
 
         #   Loop through each loop in file
         for loop in data["loops"]:
-            label = False
+            label = 0
             best_factor = 1
 
             #   positive iff any of the unroll factors cycles <= .98 * baseline cycles
@@ -40,16 +40,16 @@ def gen_labeled_data():
             medians = loop["median_cycles"]
             baseline = medians["1"] * 0.98
             if medians["2"] <= baseline:
-                label = True
+                label = 1
                 best_factor = 2
 
             if medians["4"] <= baseline:
-                label = True
+                label = 1
                 if medians["4"] < medians["2"]:
                     best_factor = 4
 
             if medians["8"] <= baseline:
-                label = True
+                label = 1
                 if medians["8"] < medians["4"] and medians["8"] < medians["2"]:
                     best_factor = 8
 
@@ -61,6 +61,8 @@ def gen_labeled_data():
             del loop["median_times_seconds"]
             del loop["median_cycles"]
             del loop["timing_stats"]
+            del loop["features"]["loop_location"]
+            del loop["features"]["loop_range"]
             
             #   Concat loop to dataset
             with open(OUTPUT_PATH, 'a', encoding="utf-8") as file:
